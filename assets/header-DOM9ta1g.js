b31d35484502a0bfdddc424de0fd5ad8dc384b62
Younghoo Nam <nyounghoo@gmail.com>
2024-12-06 20:10:29 +0900

    Updates

---
+++ assets/header-DOM9ta1g.js
@@ -0,0 +1,12 @@
+console.log("/portfolio/");const o=`<header>
+  <div class="logo">
+    <a href="/index.html">Younghoo Nam</a>
+  </div>
+  <div class="links">
+    <a href="/portfolio/index.html">Projects</a>
+    <a href="/portfolio/src/artworks/artworks.html">Artwork</a>
+    <a href="/portfolio/src/web-projects/web-projects.html">Web</a>
+    <a href="/portfolio/src/contact/contact.html">Contact</a>
+  </div>
+</header>
+`,r=document.querySelector(".wrapper");r.insertAdjacentHTML("afterbegin",o);
